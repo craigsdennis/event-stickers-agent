f7fa50a42ce587960a7e0b3c99c102474752c5a3
Craig Dennis <craigsdennis@gmail.com>
2025-05-04 02:16:15 -0700

    Thank you Claude!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,19 +4,4 @@ module.exports = {
     'index.html',
     'src/**/*.{js,ts,jsx,tsx,css}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#F38020',
-          dark: '#DC7816',
-          light: '#FFAC42',
-        },
-      },
-      fontFamily: {
-        pixel: ['"Press Start 2P"', 'monospace'],
-      },
-    },
-  },
-  plugins: [],
 };
